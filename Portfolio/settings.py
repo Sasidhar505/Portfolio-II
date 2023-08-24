93d37756ca23b65dd758179a19b72120cbb693c6
susei <panthagadasasidhar@gmail.com>
2023-08-24 23:03:49 +0530

    alternate deployments

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-$n534^7#3a3ahstvj(+h@l-*)t-az8!=h#8kh3@4lc8)t%n(e^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app' , '127.0.0.1']
 
 
 # Application definition
