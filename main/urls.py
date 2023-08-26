10c52ad5ed539b4d37340f610610c93fe5fe26aa
susei <panthagadasasidhar@gmail.com>
2023-08-26 17:54:31 +0530

    rerouting config through jinga

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/' , views.home , name='home'),
-    path('about/' , views.about , name='about'),
-    path('works/' , views.works , name='works'),
-    path('skills/' , views.skills , name='skills'),
-    path('contact/' , views.contact , name='contact'),
+    # path('home/' , views.home , name='home'),
+    # path('about/' , views.about , name='about'),
+    # path('works/' , views.works , name='works'),
+    # path('skills/' , views.skills , name='skills'),
+    # path('contact/' , views.contact , name='contact'),
     ]
--- main/views.py
+++ main/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Ma_Works
+from django.template.loader import render_to_string
 # Create your views here.
 
 
-def index(request):
-    return render(request,'index.html'  )
-
-def about(request):
-    return render(request , 'about.html')
-
-def home(request):
-    return render(request , 'home.html')
-
-def skills(request):
-    return render(request , 'skills.html')
-
-def works(request):
+def index(request ):
+    selected_item = request.GET.get('item', 'default')
     works = Ma_Works.objects.all()
-    return render(request , 'works.html' , {'works': works})
+    data = {'works': works , 'selected_item': selected_item}
+    print(request.GET)
+    return render(request, 'index.html' , data )
 
-def contact(request):
-    return render(request , 'contact.html')
+# def about(request):
+#     return render(request , 'about.html')
+
+# def home(request):
+#     return render(request , 'home.html')
+
+# def skills(request):
+#     return render(request , 'skills.html')
+
+# def works(request):
+#     works = Ma_Works.objects.all()
+#     return render(request , 'works.html' , {'works': works})
+
+# def contact(request):
+#     return render(request , 'contact.html')
