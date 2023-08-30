ec36b5c865f00e7d4e9268f5f5ee18930061f45b
susei <panthagadasasidhar@gmail.com>
2023-08-30 23:16:52 +0530

    vercel timeout debug

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = "Portfolio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sds_portfolio',
-        'USER': 'suseiboka',
-        'PASSWORD': 'SaSi2910',
-        'HOST': 'portfolio.cf4o1gigufdt.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sds_portfolio',
+#         'USER': 'suseiboka',
+#         'PASSWORD': 'SaSi2910',
+#         'HOST': 'portfolio.cf4o1gigufdt.us-east-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
@@ -137,12 +137,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-AWS_ACCESS_KEY_ID = 'AKIA2TDMGYEZAOXZZYXE'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'sdsportfolio'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'us-east-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = 'AKIA2TDMGYEZAOXZZYXE'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'sdsportfolio'
+# AWS_S3_SIGNATURE_NAME = 's3v4',
+# AWS_S3_REGION_NAME = 'us-east-1'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL =  None
+# AWS_S3_VERITY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
